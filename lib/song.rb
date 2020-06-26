01227e584c5feb2a5b681026eacc02c73fb66ad2
Ray Ockenfels <ockenfelsr@gmail.com>
2020-06-26 21:52:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,61 @@ class Song
   def save
     self.class.all << self
   end
+  
+  def self.create
+    song = self.new
+    @@all << song
+    song
+  end
+  
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+  
+  def self.create_by_name(name)
+    song = self.new_by_name(name)
+    @@all << song
+    song
+  end
+  
+  def self.find_by_name(name)
+    @@all.find{|song| song.name == name}
+  end
+  
+  def self.find_or_create_by_name(name)
+    if @@all.find_by_name(name)
+      return @@all[name]
+    else
+      song = create_by_name(name)
+      return song
+    end
+  end
+  
+  def self.alphabetical
+    @@all.sort_by {|a, b| a[0] <=> b[0]}
+  end
+  
+  def new_from_filename(file_name)
+    data = file_name.split("-")
+    title = data[1].lstrip
+    title = title.slice(0, title.length-4)
+    artist = data[0]
+    
+    song = self.new_by_name(title)
+    song.artist_name = artist
+    
+    song
+  end
+  
+  def self.create_from_filename(file_name)
+    song = self.new_from_filename(file_name)
+    @all << song
+  end
+  
+  def self.destroy_all
+    self.all.clear
+  end
 
 end
